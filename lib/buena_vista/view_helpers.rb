48c8edea3600fd036b0017d01f8c39564808fc54
Martin Kleppmann <martin@rapportive.com>
2010-07-18 18:09:05 -0700

    Tidying: move stuff into constants and separate methods

--- lib/buena_vista/view_helpers.rb
+++ lib/buena_vista/view_helpers.rb
@@ -3,81 +3,59 @@ module BuenaVista
     # For html_escape
     include ERB::Util
 
+    # An array of "disjunctive" characters which imply some degree of separation between the text
+    # preceding and the text following it. "Conjunctive" characters like ampersand and plus are
+    # not included in this list, because we would rather keep expressions like "a & b" together,
+    # if we can.
+    DISJUNCTIVE_CHARS = %w(/ \\ ~ | . < > : ; - = # _) + [
+      "\xC2\xA6",     "\xC2\xAB",     "\xC2\xB7",     "\xC2\xBB",     # broken bar, &laquo;, middle dot, &raquo;
+      "\xE2\x80\x90", "\xE2\x80\x91", "\xE2\x80\x92", "\xE2\x80\x93", # hyphen, non-breaking hyphen, figure dash, en dash
+      "\xE2\x80\x94", "\xE2\x80\x95", "\xE2\x80\x96", "\xE2\x80\xA2", # em dash, horizontal bar, double bar, bullet
+      "\xE2\x80\xA3", "\xE2\x80\xB9", "\xE2\x80\xBA"                  # triangular bullet, &lsaquo;, &rsaquo;
+    ]
+
+    # Regex matching disjunctive characters with whitespace on either side.
+    DISJUNCTIVE_CHARS_REGEX = /\s+(#{DISJUNCTIVE_CHARS.map{|char| Regexp.escape(char) }.join('|')})+\s/
+
+    # Matches stuff which looks like the start of a sentence.
+    SENTENCE_START_REGEX = /\s+[\(\[\{<'"]+\w/
+
+    # Matches stuff which looks like the end of a sentence.
+    SENTENCE_END_REGEX = /\w[\.,!\?\)\]\}>'"]+\s+/
+
+    # Mapping of different types of string split regexes to their properties.
+    SPLIT_TYPES = {
+      SENTENCE_END_REGEX      => {:cost => 1, :split => :after},  # sentence boundary
+      SENTENCE_START_REGEX    => {:cost => 1, :split => :before}, # sentence boundary
+      DISJUNCTIVE_CHARS_REGEX => {:cost => 2, :split => :before}, # punctuation surrounded by space
+      / / => {:cost => 8,   :split => :before}, # word boundary
+      /./ => {:cost => 100, :split => :before}  # mid-word
+    }
+
+
     # Splits text into two parts: a visible part and a hidden part. Useful in situations
     # where we only want to show the beginning of some text by default, and a link for
     # expanding it to display more. See specs for usage.
+    #
+    # Tries to find a human-friendly point to split the string by defining a cost function,
+    # calculating the cost of different split positions and types, and choosing the one with
+    # the lowest cost. For example, a split at a sentence boundary has a much lower cost factor
+    # than a split in the middle of a word. The cost factor is multiplied by the distance from
+    # the ideal split position.
+    #
+    #   "Customer Feedback 2.0 - Harness the ideas of your customers. Build great products."
+    #                         ^                                       ^    ^   ^---- cost = (mid-word split cost)        * 1  = 100
+    #                         |                                       |    `-------- cost = (word boundary cost)         * 5  = 50
+    #                         |                                       `------------- cost = (sentence boundary cost)     * 10 = 10
+    #                         `----------------------------------------------------- cost = (punctuation separator cost) * 50 = 100
+    #
+    # In this case, the sentence boundary split carries the lowest cost.
     def truncate_text(string_or_list_of_strings, options, &block)
       string_or_list_of_strings ||= []
       strings = string_or_list_of_strings.kind_of?(Array) ? string_or_list_of_strings : [string_or_list_of_strings]
 
       options.assert_valid_keys(:length)
       max_length = options[:length] or raise ArgumentError, "Please specify a :length option"
-
-      # The cost factor is multiplied with the distance from the ideal split position.
-      #
-      # "Customer Feedback 2.0 - Harness the ideas of your customers. Build great products."
-      #                       ^                                       ^    ^   ^---- cost = (mid-word split cost)        * 1  = 100
-      #                       |                                       |    `-------- cost = (word boundary cost)         * 5  = 50
-      #                       |                                       `------------- cost = (sentence boundary cost)     * 10 = 10
-      #                       `----------------------------------------------------- cost = (punctuation separator cost) * 50 = 100
-      #
-      # In this case, the sentence boundary split carries the lowest cost.
-
-      # "Conjunctive" characters like ampersand and plus are not included in this list, because we would
-      # rather keep expressions like "a & b" together, if we can.
-      common_separators = %w(/ \\ ~ | . < > : ; - = # _) + [
-        "\xC2\xA6",     "\xC2\xAB",     "\xC2\xB7",     "\xC2\xBB",     # broken bar, &laquo;, middle dot, &raquo;
-        "\xE2\x80\x90", "\xE2\x80\x91", "\xE2\x80\x92", "\xE2\x80\x93", # hyphen, non-breaking hyphen, figure dash, en dash
-        "\xE2\x80\x94", "\xE2\x80\x95", "\xE2\x80\x96", "\xE2\x80\xA2", # em dash, horizontal bar, double bar, bullet
-        "\xE2\x80\xA3", "\xE2\x80\xB9", "\xE2\x80\xBA"                  # triangular bullet, &lsaquo;, &rsaquo;
-      ]
-
-      common_separator_regex = /\s+(#{common_separators.map{|char| Regexp.escape(char) }.join('|')})+\s/
-
-      split_types = {
-        /\w[\.,!\?\)\]\}>'"]+\s+/ => {:cost => 1, :split => :after},  # sentence boundary
-        /\s+[\(\[\{<'"]+\w/       => {:cost => 1, :split => :before}, # sentence boundary
-        common_separator_regex    => {:cost => 2, :split => :before}, # punctuation surrounded by space
-        / / => {:cost => 8,   :split => :before}, # word boundary
-        /./ => {:cost => 100, :split => :before}  # mid-word
-      }
-
-      # Searches for matches of `regex` within `text` and returns two strings:
-      # 1. all the text before a match; 2. the matched text itself plus everything thereafter.
-      # The split position is the last match in `text` which obeys the condition that
-      # the length of the first returned string must not be longer than `limit`.
-      # If regex isn't found, returns ['', text].
-      def split_before_match(text, regex, limit)
-        raise ArgumentError, "limit should be less than text length" if limit >= text.size
-        before, separator, after = '', '', text
-
-        while true
-          match = after.match(regex)
-          break if !match || before.size + separator.size + match.pre_match.size > limit
-          before << separator << match.pre_match
-          separator = match.to_s
-          after = match.post_match
-        end
-        [before, separator + after]
-      end
-
-      # Searches for matches of `regex` within `text` and returns two strings:
-      # 1. all the text before a match plus the matched text itself; 2. everything thereafter.
-      # The split position is the last match in `text` which obeys the condition that
-      # the length of the first returned string must not be longer than `limit`.
-      # If regex isn't found, returns ['', text].
-      def split_after_match(text, regex, limit)
-        raise ArgumentError, "limit should be less than text length" if limit >= text.size
-        before, after = '', text
-
-        while true
-          match = after.match(regex)
-          break if !match || before.size + match.pre_match.size > limit
-          before << match.pre_match << match.to_s
-          after = match.post_match
-        end
-        [before, after]
-      end
 
       first_block = true
 
@@ -91,7 +69,7 @@ module BuenaVista
 
         else
           # Try each type of split, and calculate the cost; then pick the type with the lowest cost.
-          split = split_types.map do |regex, options|
+          split = SPLIT_TYPES.map do |regex, options|
             before, after = if options[:split] == :before
               split_before_match(text, regex, max_length)
             else
@@ -140,6 +118,45 @@ module BuenaVista
         truncated_class = ' class="truncated"' if !block[:visible]
         "<#{block_tag}#{truncated_class}>#{block[:html].join}</#{block_tag}>"
       end.join
+    end
+
+    private
+
+    # Searches for matches of `regex` within `text` and returns two strings:
+    # 1. all the text before a match; 2. the matched text itself plus everything thereafter.
+    # The split position is the last match in `text` which obeys the condition that
+    # the length of the first returned string must not be longer than `limit`.
+    # If regex isn't found, returns ['', text].
+    def split_before_match(text, regex, limit)
+      raise ArgumentError, "limit should be less than text length" if limit >= text.size
+      before, separator, after = '', '', text
+
+      while true
+        match = after.match(regex)
+        break if !match || before.size + separator.size + match.pre_match.size > limit
+        before << separator << match.pre_match
+        separator = match.to_s
+        after = match.post_match
+      end
+      [before, separator + after]
+    end
+
+    # Searches for matches of `regex` within `text` and returns two strings:
+    # 1. all the text before a match plus the matched text itself; 2. everything thereafter.
+    # The split position is the last match in `text` which obeys the condition that
+    # the length of the first returned string must not be longer than `limit`.
+    # If regex isn't found, returns ['', text].
+    def split_after_match(text, regex, limit)
+      raise ArgumentError, "limit should be less than text length" if limit >= text.size
+      before, after = '', text
+
+      while true
+        match = after.match(regex)
+        break if !match || before.size + match.pre_match.size > limit
+        before << match.pre_match << match.to_s
+        after = match.post_match
+      end
+      [before, after]
     end
   end
 end
